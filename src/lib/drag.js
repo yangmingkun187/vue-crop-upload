de49d6ed529cac81339381b849ff17df4e107f1e
jason yang <jasonyang187@outlook.com>
2017-03-16 10:50:26 +0800

    优化drag函数的逻辑

--- src/lib/drag.js
+++ src/lib/drag.js
@@ -1,4 +1,7 @@
-let moves,moveEnd;
+let moves,
+	moveEnd,
+	translateX = 0,
+	translateY = 0;
 
 export default class Drag {
 	
@@ -7,6 +10,11 @@ export default class Drag {
 		const _self = this;
 		_self.originImage = $originImage;
 		_self.visibility = $visibility;
+		
+		_self.startPosition = {
+			x: e.touches[0].clientX,
+			y: e.touches[0].clientY
+		};
 		
 		_self.originPosition = {
 			top: _self.originImage.offsetTop,
@@ -22,17 +30,13 @@ export default class Drag {
 			right: _self.visibility.offsetLeft + _self.visibility.offsetWidth
 		};
 		
-		_self.startPosition = {
-			x: e.touches[0].clientX,
-			y: e.touches[0].clientY,
-		};
 		_self.originImage.style.transitionTimingFunction = '';
 		_self.originImage.style.transitionDuration = '';
 		
-		moves = function(e) {
+		moves = function (e) {
 			_self.move(e);
 		};
-		moveEnd = function(e) {
+		moveEnd = function (e) {
 			_self.stopMove(e);
 		};
 		
@@ -51,8 +55,8 @@ export default class Drag {
 		};
 		
 		let offset = {
-			x: movePosition.x - _self.startPosition.x,
-			y: movePosition.y - _self.startPosition.y
+			x: translateX + movePosition.x - _self.startPosition.x,
+			y: translateY + movePosition.y - _self.startPosition.y
 		};
 		
 		_self.originImage.style.transform = 'translate3d(' + offset.x + 'px,' + offset.y + 'px,0)';
@@ -67,80 +71,74 @@ export default class Drag {
 		};
 		
 		let stopOffset = {
-			x: stopPosition.x - _self.startPosition.x,
-			y: stopPosition.y - _self.startPosition.y
+			x: translateX + stopPosition.x - _self.startPosition.x,
+			y: translateY + stopPosition.y - _self.startPosition.y
 		};
 		
 		let xCache = 0, yCache = 0;
 		
 		
 		// 右下角滑动
-		if(stopOffset.x > 0 && stopOffset.y > 0) {
+		if (stopOffset.x >= 0 && stopOffset.y >= 0) {
 			
-			if(_self.originPosition.left + stopOffset.x > _self.offsetRange.left) {
+			if (_self.originPosition.left + stopOffset.x > _self.offsetRange.left) {
 				xCache = _self.offsetRange.left;
 			} else {
 				xCache = stopOffset.x;
 			}
-			if(_self.originPosition.top + stopOffset.y > _self.offsetRange.top) {
+			if (_self.originPosition.top + stopOffset.y > _self.offsetRange.top) {
 				yCache = _self.offsetRange.top - _self.originPosition.top;
 			} else {
 				yCache = stopOffset.y
 			}
-			_self.originImage.style.transform = 'translate3d('+xCache+'px,'+yCache+'px,0)';
-		}
-		
-		// 右上角滑动
-		if(stopOffset.x > 0 && stopOffset.y < 0) {
 			
-			if(_self.originPosition.left + stopOffset.x > _self.offsetRange.left) {
+		} else if (stopOffset.x >= 0 && stopOffset.y <= 0) {		// 右上角滑动
+			
+			if (_self.originPosition.left + stopOffset.x > _self.offsetRange.left) {
 				xCache = _self.offsetRange.left;
 			} else {
 				xCache = stopOffset.x;
 			}
-			if(_self.originPosition.bottom + stopOffset.y < _self.offsetRange.bottom) {
+			if (_self.originPosition.bottom + stopOffset.y < _self.offsetRange.bottom) {
 				yCache = _self.offsetRange.bottom - _self.originPosition.bottom;
 			} else {
 				yCache = stopOffset.y
 			}
-			_self.originImage.style.transform = 'translate3d('+xCache+'px,'+yCache+'px,0)';
-		}
-		
-		// 左下角滑动
-		if(stopOffset.x < 0 && stopOffset.y > 0) {
 			
-			if(_self.originPosition.right + stopOffset.x < _self.offsetRange.right) {
+		} else if (stopOffset.x <= 0 && stopOffset.y >= 0) {// 左下角滑动
+			
+			if (_self.originPosition.right + stopOffset.x < _self.offsetRange.right) {
 				xCache = _self.offsetRange.left;
 			} else {
 				xCache = stopOffset.x;
 			}
-			if(_self.originPosition.top + stopOffset.y > _self.offsetRange.top) {
+			if (_self.originPosition.top + stopOffset.y > _self.offsetRange.top) {
 				yCache = _self.offsetRange.top - _self.originPosition.top;
 			} else {
 				yCache = stopOffset.y
 			}
-			_self.originImage.style.transform = 'translate3d('+xCache+'px,'+yCache+'px,0)';
-		}
-		
-		// 左上角滑动
-		if(stopOffset.x < 0 && stopOffset.y < 0) {
 			
-			if(_self.originPosition.right + stopOffset.x < _self.offsetRange.right) {
+		} else if (stopOffset.x <= 0 && stopOffset.y <= 0) {// 左上角滑动
+			
+			if (_self.originPosition.right + stopOffset.x < _self.offsetRange.right) {
 				xCache = _self.offsetRange.left;
 			} else {
 				xCache = stopOffset.x;
 			}
-			if(_self.originPosition.bottom + stopOffset.y < _self.offsetRange.bottom) {
+			if (_self.originPosition.bottom + stopOffset.y < _self.offsetRange.bottom) {
 				yCache = _self.offsetRange.bottom - _self.originPosition.bottom;
 			} else {
 				yCache = stopOffset.y
 			}
-			_self.originImage.style.transform = 'translate3d('+xCache+'px,'+yCache+'px,0)';
 		}
 		
+		translateX = xCache;
+		translateY = yCache;
+		
+		_self.originImage.style.transform = 'translate3d(' + xCache + 'px,' + yCache + 'px,0)';
 		_self.originImage.style.transitionTimingFunction = 'cubic-bezier(0.1, 0.57, 0.1, 1)';
 		_self.originImage.style.transitionDuration = '700ms';
-
+		
 		document.removeEventListener('touchmove', moves, false);
 		document.removeEventListener('touchend', moveEnd, false);
 	}
