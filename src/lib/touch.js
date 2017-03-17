2c58b74943aa6ba4b6a43da767281df8e0d8a97e
jason yang <jasonyang187@outlook.com>
2017-03-17 18:34:42 +0800

    重构

--- src/lib/touch.js
+++ src/lib/touch.js
@@ -3,15 +3,19 @@ import Hammer from 'hammerjs';
 export default class Touch {
 	
 	constructor($container,$imageWrapper) {
+		
+		if (!Hammer) {
+			throw new Error('[vue-touch] cannot locate Hammer.js.')
+		}
 	
 	  let HM = new Hammer($container);
 	
 	  // visibility zone
 	  let offsetRange = {
+		  width: $container.offsetWidth,
+		  height: $container.offsetHeight,
 		  top: $container.offsetTop,
-		  left: $container.offsetLeft,
-		  bottom: $container.offsetTop + $container.offsetHeight,
-		  right: $container.offsetLeft + $container.offsetWidth
+		  left: $container.offsetLeft
 	  };
 	
 	  // 创建图片的虚拟dom，使用transform做位移，避免重绘
@@ -19,9 +23,7 @@ export default class Touch {
 		  width: $imageWrapper.offsetWidth,
 		  height: $imageWrapper.offsetHeight,
 		  top: $imageWrapper.offsetTop,
-		  left: $imageWrapper.offsetLeft,
-		  bottom: $imageWrapper.offsetTop + $imageWrapper.offsetHeight,
-		  right: $imageWrapper.offsetLeft + $imageWrapper.offsetWidth
+		  left: $imageWrapper.offsetLeft
 	  };
 	
 	  HM.get('pinch').set({ enable: true });
@@ -36,9 +38,12 @@ export default class Touch {
   
 	pan($imageWrapper,offsetRange,virtualImage) {
 		
-		let translateX = 0,
-			translateY = 0,
-			translate3dStr = 'translate3d(0,0,0) ',
+		let transformer = {
+			x: 0,
+			y: 0
+		};
+		
+		let translate3dStr = 'translate3d(0,0,0) ',
 			scaleStr = 'scale(1,1)';
 		
 		let start = function(ev) {
@@ -52,77 +57,45 @@ export default class Touch {
 		
 		let move = function(ev) {
 			let offset = {
-				x: translateX + ev.deltaX,
-				y: translateY + ev.deltaY
+				x: transformer.x + ev.deltaX,
+				y: transformer.y + ev.deltaY
 			};
 			translate3dStr = 'translate3d(' + offset.x + 'px,' + offset.y + 'px,0) ';
 			$imageWrapper.style.transform = translate3dStr + scaleStr;
 		};
 		
 		let end = function(ev) {
-			let xCache = 0, yCache = 0;
-			// 右下角滑动
 			
-			if (ev.deltaX >= 0 && ev.deltaY >= 0) {
-				
-				if (virtualImage.left + ev.deltaX > offsetRange.left) {
-					xCache = offsetRange.left;
-				} else {
-					xCache = ev.deltaX;
-				}
-				if (virtualImage.top + ev.deltaY > offsetRange.top) {
-					yCache = ev.deltaY
-				} else {
-					yCache = offsetRange.top - virtualImage.top;
-				}
-				
-			} else if (ev.deltaX >= 0 && ev.deltaY <= 0) {		// 右上角滑动
-				
-				if (virtualImage.left + ev.deltaX > offsetRange.left) {
-					xCache = offsetRange.left;
-				} else {
-					xCache = ev.deltaX;
-				}
-				if (virtualImage.bottom + ev.deltaY < offsetRange.bottom) {
-					yCache = offsetRange.bottom - virtualImage.bottom;
-				} else {
-					yCache = ev.deltaY
-				}
-				
-			} else if (ev.deltaX <= 0 && ev.deltaY >= 0) {// 左下角滑动
-				
-				if (virtualImage.right + ev.deltaX < offsetRange.right) {
-					xCache = offsetRange.left;
-				} else {
-					xCache = ev.deltaX;
-				}
-				if (virtualImage.top + ev.deltaY > offsetRange.top) {
-					yCache = offsetRange.top - virtualImage.top;
-				} else {
-					yCache = ev.deltaY
-				}
-				
-			} else if (ev.deltaX <= 0 && ev.deltaY <= 0) {// 左上角滑动
-				
-				if (virtualImage.right + ev.deltaX < offsetRange.right) {
-					xCache = offsetRange.left;
-				} else {
-					xCache = ev.deltaX;
-				}
-				if (virtualImage.bottom + ev.deltaY < offsetRange.bottom) {
-					yCache = offsetRange.bottom - virtualImage.bottom;
-				} else {
-					yCache = ev.deltaY
-				}
+			let maxX = offsetRange.left - virtualImage.left;
+			let maxY = offsetRange.top - virtualImage.top;
+			
+			if(maxX < 0 || maxY < 0) {
+				$imageWrapper.style.transform = 'translate3d(0,0,0) scale(1,1)';
+				$imageWrapper.style.transitionTimingFunction = 'cubic-bezier(0.1, 0.57, 0.1, 1)';
+				$imageWrapper.style.transitionDuration = '700ms';
+				return;
 			}
 			
-			translateX = xCache;
-			translateY = yCache;
-			translate3dStr = 'translate3d(' + xCache + 'px,' + yCache + 'px,0) ';
+			if(transformer.x + Math.abs(ev.deltaX) > maxX) {
+				transformer.x += ev.deltaX > 0 ? maxX : -maxX;
+			} else {
+				transformer.x += ev.deltaX;
+			}
+			
+			if(transformer.y + Math.abs(ev.deltaY) > maxY) {
+				transformer.y += ev.deltaY > 0 ? maxY : -maxY;
+			} else {
+				transformer.y += ev.deltaY;
+			}
+			
+			translate3dStr = 'translate3d(' + transformer.x + 'px,' + transformer.y + 'px,0) ';
 			
 			$imageWrapper.style.transform = translate3dStr + scaleStr;
 			$imageWrapper.style.transitionTimingFunction = 'cubic-bezier(0.1, 0.57, 0.1, 1)';
 			$imageWrapper.style.transitionDuration = '700ms';
+			
+			virtualImage.top += transformer.y;
+			virtualImage.left += transformer.x;
 		};
 		
 		return {
